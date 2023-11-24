8c0d8a5a1e378266c705ae624c5a3f872880ff9a
Saeedmora <76522495+Saeedmora@users.noreply.github.com>
2023-11-24 02:05:35 +0100

    productscrenn funktionfähig

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -10,9 +10,9 @@ const ProductScreen = () => {
   const product = products.find((p)=> p._id===productId);
  
 
-  return 
-    <>
-    <Link className='btn btn-light my-3' to='/'>
+  return (
+   <>
+     <Link className='btn btn-light my-3' to='/'>
       GO Back
     </Link>
     <Row>
@@ -29,13 +29,16 @@ const ProductScreen = () => {
           <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
         </ListGroup.Item>
         <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+        
+        <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
        </ListGroup>
+       
       </Col>
 
       <Col md={3}>
         <Card>
           <ListGroup>
-           <ListGroup.Item>
+          <ListGroup.Item>
              <Row>
                <Col>Price: </Col>
               
@@ -43,7 +46,22 @@ const ProductScreen = () => {
                 <strong>${product.price}</strong>
                </Col>
              </Row>
-   
+            </ListGroup.Item>
+           <ListGroup.Item>
+             <Row>
+               <Col>Status </Col>
+              
+               <Col>
+                <strong>${product.countInStock > 0?'In Stock ' :'Out of Stock'}</strong>
+               </Col>
+             </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+               className='btn-block'
+               type = 'button'
+               disabled={product.countInStock===0}
+               >Add to Cart</Button>
             </ListGroup.Item>
 
           </ListGroup>
@@ -52,8 +70,8 @@ const ProductScreen = () => {
         
       </Col>
     </Row>
-    </>;
-  
+    </>
+  );
 };
 
 export default ProductScreen
