13a8079e0e69cc6ccf1385f385501dda760790d5
Saeedmora <76522495+Saeedmora@users.noreply.github.com>
2023-12-03 17:13:37 +0100

    models 2

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,6 +21,6 @@ const userSchema = new mongoose.Schema({
     required:true,
     default:false,
   },
-},{timestamps:true});
+},{timestampes:true,});
 const User = mongoose.model("User",userSchema);
 export default User;
